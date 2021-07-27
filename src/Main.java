8de36bf2778216f1742b51e4267c7050212738af
Tatiana Lukina <tanyalykina57@gmail.com>
2021-07-27 18:50:05 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 20_0000;
+        int miles = 1;
+        int cost = 20;
+        int bonus= ticket/cost*miles;
+                System.out.println("total"+ bonus + "miles");
+
+    }
+
+
+
+}
